c4296459da50156be6d0a6dfb5b7fba709d7f009
DenisM1111 <deniska7108@mail.ru>
2024-02-27 14:34:56 +0300

    db

--- postgress.py
+++ postgress.py
@@ -1,9 +1,34 @@
 import psycopg2
 con=psycopg2.connect(dbname='test', user='postgres', password='Q1w2e3r4', host='localhost')
 cursor=con.cursor()
-#qwery='SELECT login, password, id FROM public."logins"'
+table="logins"
+def select(columns,wcolumns,value):
+    global cursor
+    global table
+    if len(wcolumns)>0:
+        qwery = f'SELECT {columns} FROM public.{table} WHERE {wcolumns}={value}'
+    else:
+        qwery = f'SELECT {columns} FROM public.{table}'
+    cursor.execute(qwery)
+    data = cursor.fetchall()
+    return data
+def update(columns,wcolumns,value,wvalue):
+    global cursor
+    global table
+    if len(wcolumns)>0:
+        qwery = f'UPDATE public.{table} SET {columns} WHERE {wcolumns}={wvalue};'
+    else:
+        qwery = f"UPDATE public.{table} SET {columns}={value};'
+    cursor.execute(qwery)
+    con.commit()
+    return 0
+def insert(columns,values):
+    global cursor
+    global table
+
+
 qwery="UPDATE public.logins SET password='138871263' WHERE id=2;"
 cursor.execute(qwery)
 con.commit()
-data=cursor.fetchall()
+data=cursor.fetchone()
 print(data)
